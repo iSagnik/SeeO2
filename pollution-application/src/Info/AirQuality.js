abb2351f8d4da4f674719454ed395952092c473a
isagnik <isagnikbhattacharjee@gmail.com>
2020-10-17 21:12:46 -0400

    Weather widget updates

--- pollution-application/src/Info/AirQuality.js
+++ pollution-application/src/Info/AirQuality.js
@@ -1,3 +1,40 @@
-// API key call
-// http://api.airpollutionapi.com/1.0/aqi?lat={LATITUDE}&lon={LONGITUDE}&APPID={APIKEY}
+//API: https://api.airvisual.com/v2/city?city=Los%20Angeles&state=California&country=USA&key=08662725-f7b9-4eaa-9a86-6d70bd578f99
 
+import React, { useState, useEffect } from "react";
+import axios from "axios"
+
+const url = "https://api.airvisual.com/v2/city?city=Los%20Angeles&state=California&country=USA&key=08662725-f7b9-4eaa-9a86-6d70bd578f99"
+
+
+const AirQualityWidget = () => {
+    // const [temp, setTemp] = useState(null)
+    // const [humid, setHumid] = useState(null)
+    // const [speed, setSpeed] = useState(null)
+    // const [precip, setCloud] = useState(null)
+    // const [desc, setDesc] = useState(null)
+    const [aqi, setAqi] = useState(null)
+
+    const fetch = async () => {
+        console.log(url)
+        const request = axios.get(url)
+        const response = await request
+
+        setAqi(response.data.data.current.pollution.aqius)
+        // setTemp(response.data.main.temp)
+        // setHumid(response.data.main.humidity)
+        // setSpeed(response.data.wind.speed) 
+        // setCloud(response.data.clouds.all)
+        // setDesc(response.data.weather[0].description)
+    }   
+
+    useEffect(() => {
+        fetch()
+
+    }, [])
+    
+    return (
+        <p>{JSON.stringify(aqi)}</p>
+    )
+}
+
+export default AirQualityWidget
--- pollution-application/src/Info/Weather.js
+++ pollution-application/src/Info/Weather.js
@@ -38,7 +38,7 @@ const WeatherWidget = (props) => {
                 <div class="weather-gradient"></div>
                 <div class="date-container">
                     <h2 class="date-dayname">Tuesday</h2><span class="date-day">{props.Date}</span>
-                    <i class="location-icon" data-feather="map-pin"></i>
+                    <span><FontAwesomeIcon icon = {faMapMarkerAlt} size = "1x" /></span>
                     <span class="location">{props.City}</span></div>
                 <div class="weather-container">
                     <i class="weather-icon" data-feather="sun"></i>
@@ -49,7 +49,7 @@ const WeatherWidget = (props) => {
             <div class="info-side">
                 <div class="today-info-container">
                     <div class="today-info">
-                        <div class="precipitation"> <span class="title">PRECIPITATION</span><span class="value">{precip} %</span>
+                        <div class="precipitation"> <span class="title">PRECIPITATION  </span><span class="value">{precip} %</span>
                             <div class="clear"></div>
                         </div>
                         <div class="humidity"> <span class="title">HUMIDITY</span><span class="value">{humid} %</span>
@@ -60,7 +60,7 @@ const WeatherWidget = (props) => {
                         </div>
                     </div>
                 </div>
-                <div class="week-container">
+                {/* <div class="week-container">
                     <ul class="week-list">
                         <li class="active"><i class="day-icon" data-feather="sun"></i><span class="day-name">Tue</span><span class="day-temp">29°C</span></li>
                         <li><i class="day-icon" data-feather="cloud"></i><span class="day-name">Wed</span><span class="day-temp">21°C</span></li>
@@ -68,11 +68,11 @@ const WeatherWidget = (props) => {
                         <li><i class="day-icon" data-feather="cloud-rain"></i><span class="day-name">Fry</span><span class="day-temp">19°C</span></li>
                         <div class="clear"></div>
                     </ul>
-                </div>
+                </div> */}
                 <div class="location-container">
                     <button class="location-button">
-                        <i data-feather="map-pin"></i>
-                        <span>Change location</span>
+                        <span><FontAwesomeIcon icon = {faMapMarkerAlt} size = "1x" /></span>
+                        <span>   Change location</span>
                     </button>
                 </div>
             </div>
